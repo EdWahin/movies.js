e73003079bdf30b69b0397133b98f3aa9808b99f
Eduard <eduardvagin14@gmail.com>
2022-09-30 21:22:52 +0300

    start using cycle and condition

--- js/script.js
+++ js/script.js
@@ -8,12 +8,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('What was the last movie you watched?', ''),
-      b = prompt('How much would you rate it?', ''),
-      c = prompt('What was the last movie you watched?', ''),
-      d = prompt('How much would you rate it?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('What was the last movie you watched?', ''),
+          b = prompt('How much would you rate it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Watched quite a few films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You are a classic viewer');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are a movie buff');
+} else {
+    console.log('Error');
+}
 
 console.log(personalMovieDB);
